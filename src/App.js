1451933b62840d1228b0e2c1dfc7ae84b4bfe713
kateFrontend <ecdanilova@gmail.com>
2022-11-26 12:48:55 -0500

    set up buttons

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import Buttons from "./Buttons";
 
 function App() {
   const [clothes, setClothes] = useState(data);
+  
+  const chosenClothes = (searchTerm) => {
+    const newClothes = data.filter(element => element.searchTerm === searchTerm);
+    setClothes(newClothes);
+  }
 
   return (
     <div>
       <div className="cont">
         <h2 className="back">Free Standart Shipping</h2>
       </div>
-      <Buttons/>
+      <Buttons filteredClothes={chosenClothes}/>
       <Clothes itemsForSale={clothes} />
     </div>
   );
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Buttons() {
+function Buttons({filteredClothes}) {
   return (
     <div className='cont'>
-        <button className='change'>Dresses</button>
-        <button className='change'>Skirts</button>
-        <button className='change'>Pants</button>
-        <button className='change'>Shoes</button>
-        <button className='change'>Shirts</button>
+        <button className='change' onClick={() => filteredClothes("dress")}>Dresses</button>
+        <button className='change' onClick={() => filteredClothes("skirt")}>Skirts</button>
+        <button className='change' onClick={() => filteredClothes("pants")}>Pants</button>
+        <button className='change' onClick={() => filteredClothes("shoes")}>Shoes</button>
+        <button className='change' onClick={() => filteredClothes("shirt")}>Shirts</button>
     </div>
   )
 }

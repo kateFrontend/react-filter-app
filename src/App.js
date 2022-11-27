83e11c5aec2343a25728f986e483f070099ad8a9
kateFrontend <ecdanilova@gmail.com>
2022-11-27 18:05:05 -0500

    install router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import Home from './Home';
+import About from "./About";
+import Contact from "./Contact";
 import "./App.css";
 
 
 function App() {
+return (
+  <Router>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contact'>Contact</Link>
+    </nav>
 
+<Routes>
+  <Route path='/' element={ <Home /> } />
+  <Route path='/about' element={ <About /> } />
+  <Route path='/contact' element={ <Contact /> } />
+
+</Routes>
+
+  </Router>
+)
 
 }
 

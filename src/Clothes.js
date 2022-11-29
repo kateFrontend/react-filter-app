f054e7ef1f3d3caf4a3739fe32646983c04a6a6e
kateFrontend <ecdanilova@gmail.com>
2022-11-29 18:45:00 -0500

    update style

--- src/Clothes.js
+++ src/Clothes.js
@@ -7,7 +7,7 @@ function Clothes({ itemsForSale }) {
         const { id, name, searchTerm, price, image } = element;
         return (
           <div className="product-card" key={id}>
-            <img src={image} width="400px" height="500px" />
+            <img src={image} />
             <div className="product-info">
               <h3>{name}</h3>
               <h4>$ {price}</h4>
